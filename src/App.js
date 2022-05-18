0efff2a90caf932433e75fd3de4d429ff432911b
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-05-18 23:24:57 +0600

    Initialization done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './Compunents/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className='container mx-auto'>
+      <Navbar></Navbar>
+      <Routes>
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
